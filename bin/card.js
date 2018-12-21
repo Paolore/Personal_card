e3aadd2a84d0201ed8a7d499a2a75e83bc449ef8
Lorena.Correa <paolorena3@gmail.com>
2018-12-21 16:45:30 -0500

    initial commit

--- bin/card.js
+++ bin/card.js
@@ -7,25 +7,27 @@ const path = require('path')
 
 // Info
 const data = {
-  name: chalk.white.bold('Adrian Estrada'),
-  bio: chalk.white.bold('Developer, Teacher and Community Organizer'),
-  handle: chalk.cyan('@edsadr'),
-  work: chalk.white('Engineering Support Manager - NodeSource'),
-  twitter: chalk.cyan('https://twitter.com/edsadr'),
-  github: chalk.cyan('https://github.com/edsadr'),
-  linkedin: chalk.cyan('https://www.linkedin.com/in/edsadr'),
-  npx: chalk.white('npx edsadr'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelTwitter: chalk.white.bold('   Twitter:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelCard: chalk.white.bold('      Card:')
+  name: chalk.magenta.bold('Lorena Correa'),
+  bio: chalk.magenta.bold('Web Developer with tools PHP, Node, React, Angular'),
+  bio1: chalk.magenta.bold('"Nothin is impossible to the willing mind"'),
+  handle: chalk.cyan('@lavadani'),
+  work: chalk.white('Frontend Developer at Tekus'),
+  twitter: chalk.cyan('https://twitter.com/lavadani'),
+  github: chalk.cyan('https://github.com/paolore'),
+  linkedin: chalk.cyan('https://www.linkedin.com/in/1paolorena'),
+  npx: chalk.white('npx paolorena'),
+  labelWork: chalk.magenta.bold('      Work:'),
+  labelTwitter: chalk.magenta.bold('   Twitter:'),
+  labelGitHub: chalk.magenta.bold('    GitHub:'),
+  labelLinkedIn: chalk.magenta.bold('  LinkedIn:'),
+  labelCard: chalk.magenta.bold('      Card:')
 }
 
 const table = new Table()
 const newline = '\n'
 const heading = `${data.name} - ${data.handle}`
 const bio = `${data.bio}`
+const bio1 = `${data.bio1}`
 const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
@@ -33,12 +35,12 @@ const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 const getAvatar = async () => {
-  const image = path.join(__dirname, '..', 'assets', 'avatar.jpg')
+  const image = path.join(__dirname, '..', 'assets', 'images.jpg')
   let img
   try {
     img = await asciify(image, {
       fit: 'box',
-      width: '30%'
+      width: '20%'
     })
   } catch (error) {
     console.log(error.message)
@@ -50,7 +52,7 @@ const getAvatar = async () => {
 
 async function getOutput () {
   const avatar = await getAvatar()
-  const output = newline + newline + heading + newline + bio + newline.repeat(2) + working + newline + twittering + newline + githubing + newline + newline + linkedining + newline + newline + carding
+  const output = newline + newline + heading + newline + bio + newline + bio1 + newline.repeat(2) + working + newline + twittering + newline + githubing + newline + newline + linkedining + newline + newline + carding
 
   table.push(
     [avatar, { content: output, vAlign: 'center' }]
